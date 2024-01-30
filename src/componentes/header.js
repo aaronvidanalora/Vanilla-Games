5c2f8b86f41bc556de02d74b6c73b69d2f6f9652
aaronvidanalora <vidanaloraaaron@fpllefia.com>
2024-01-30 19:57:47 +0100

    enrutador #15

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,20 +1,22 @@
 // importamos la función ls del archivo funciones
 import { ls } from '../componentes/funciones'
+import { menuRol, menuUsuario } from './menus'
+import { editarPerfil } from './editarPerfil'
+
 export const header = {
   template: // html
   `
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
-  <a class="navbar-brand" href="#/home"
-  ><img
-    src="images/logo.svg"
-    alt=""
-    width="30"
-    height="24"
-    class="d-inline-block align-text-top router-link"
-  />
-  Vanilla Games</a
->
+    <a class="navbar-brand" href="#/home"
+      ><img
+        src="images/logo.svg"
+        alt=""
+        width="30"
+        height="24"
+        class="d-inline-block align-text-top"
+      />
+      Vanilla Games</a
     >
     <button
       class="navbar-toggler"
@@ -28,49 +30,67 @@ export const header = {
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a class="nav-link active router-link" aria-current="page" href="#/home">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link router-link" aria-current="page" href="#">TOP5 Proyectos</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link router-link" aria-current="page" href="#">A cerca de</a>
-    </li>
-  </ul>
-  <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a class="ms-2 btn btn-success router-link" href="#/login">
-        Iniciar sesión
-        <i class="bi bi-box-arrow-in-right"></i>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="ms-2 btn btn-outline-light router-link" href="#/registro">
-        Regístrate
-        <i class="bi bi-box-arrow-in-right"></i>
-      </a>
-    </li>
-  </ul>
+      
+      <!-- Menu común para todos los usuarios -->
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">A cerca de</a>
+        </li>
+      </ul>
+
+      <!-- Aquí va el Menu rol -->
+      <div id="menuRol"></div>
+    
+      <!-- Aquí va el Menu usuario -->
+      <div id="menuUsuario"></div>
+
+      <div id="modal">
+    <!-- Aquí inyectamos el componente editarPerfil -->
+    </div>
     </div>
   </div>
 </nav>
 
   `,
 
+  // ...
   script: () => {
     console.log('Header cargado')
+    document.querySelector('#modal').innerHTML = editarPerfil.template
     // Simulamos el inicio de sesión de un usuario
-    const usuario = {
-      email: 'manolito@email.com',
-      rol: 'alumno'
-    }
-    ls.setUsuario(usuario)
-    console.log('usuario guardado')
+    ls.setUsuario({ email: 'chafardera@gmial.com', rol: 'registrado' })
+    const rolUsuario = ls.getUsuario().rol
 
-    // Leemos el usuario del localstorage
-    const usuarioLogueado = ls.getUsuario()
-    console.log('usuario del localstorage: ', usuarioLogueado)
+    switch (rolUsuario) {
+      case 'registrado':
+      // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateRegistrado
+        // menú usuario
+        document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateRegistrado
+        break
+      case 'desarrollador':
+      // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateDesarrollador
+        // menú usuario
+        document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateDesarrollador
+        break
+      case 'admin':
+      // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateAdmin
+        // menú usuario
+        document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateAdmin
+        break
+      default : // Para usuarios anónimos
+      // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateAnonimo
+        // menú usuario: No tiene
+        break
+    }
   }
 }
